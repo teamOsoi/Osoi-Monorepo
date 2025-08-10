17f6da9645b44443b4c046ef7add06aa3bdd1717
teamOsoi <aimodeltwo@gmail.com>
2025-08-10 16:04:13 +0300

    Refactor index.js to implement server-side redirect to /home3 and remove existing components

--- politian/pages/index.js
+++ politian/pages/index.js
@@ -1,40 +1,16 @@
-import React, {Fragment} from 'react';
-import Navbar from '../components/Navbar/Navbar'
-import Hero from '../components/hero/hero';
-import Features from '../components/Features/Features';
-import About from '../components/about/about';
-import ServiceSection from '../components/ServiceSection/ServiceSection';
-import CampaignSection from '../components/CampaignSection/CampaignSection';
-import Testimonial from '../components/Testimonial/Testimonial';
-import FunFact from '../components/FunFact/FunFact';
-import TeamSection from '../components/TeamSection/TeamSection';
-import BlogSection from '../components/BlogSection/BlogSection';
-import InstagramSection from '../components/InstagramSection/InstagramSection';
-import Donors from '../components/Donors/Donors';
-import PartnerSection from '../components/PartnerSection/PartnerSection';
-import abimg from '/public/images/about.jpg'
-import Footer from '../components/footer/Footer';
-import Scrollbar from '../components/scrollbar/scrollbar';
+// pages/index.js
 
-const HomePage =() => {
-    return(
-        <Fragment>
-            <Navbar hclass={'wpo-site-header-s1'} topbarNone={'topbar-none'} Logo={'/images/logo.svg'}/>
-            <Hero/>
-            <Features/>
-            <About abimg={abimg}/>
-            <ServiceSection/>
-            <CampaignSection/>
-            <Testimonial/>
-            <FunFact/>
-            <TeamSection/>
-            <Donors/>
-            <BlogSection/>
-            <InstagramSection/>
-            <PartnerSection/>
-            <Footer/> 
-            <Scrollbar/>
-        </Fragment>
-    )
-};
-export default HomePage;
+export async function getServerSideProps() {
+    return {
+      redirect: {
+        destination: '/home3',
+        permanent: false, // change to true if this will always be the default
+      },
+    };
+  }
+  
+  export default function Index() {
+    // This component will never be shown because of the redirect
+    return null;
+  }
+  
